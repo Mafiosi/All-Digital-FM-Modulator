62dbf4febce75eb75d646f323689131d0f0773c0
Joao Beleza <joao.beleza7@gmail.com>
2018-12-04 15:35:26 +0000

    Added first dds_tb and dds

---
+++ ise/LP3/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/Faculdade/5_ano/1_semestre/PSDI/PRATICA/PSDI_TP3/ise/LP3/dds_testbench_isim_beh.exe" -prj "D:/Faculdade/5_ano/1_semestre/PSDI/PRATICA/PSDI_TP3/ise/LP3/dds_testbench_beh.prj" "work.dds_testbench" "work.glbl" 
---
+++ ise/LP3/src/dds.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module dds #(NBITS = 13, NBITS_SINE_LUT = 7, N_OUTPUT_BITS = 9, NSAMPLES_LUT = 128, HEXVAL = "../simdata/DDSLUT.hex") (
+      input clock,
+      input reset,
+      input enableclk,
+      input [NBITS-1:0] phaseinc,
+      output reg [31:0] outsine
+      );
+
+// Defines LUT block
+reg [31:0] sineLUT [0:NSAMPLES_LUT-1];
+initial $readmemh(HEXVAL, sineLUT);
+
+// Defines FF for phase calculation input of LUT
+reg [NBITS-1:0] phase;
+always @(posedge clock)
+begin
+    if (reset) begin
+        phase <= 0;
+    end else if (enableclk) begin
+        phase <= phase + phaseinc;
+    end
+    outsine <= sineLUT[ phase[NBITS-1:NBITS-NBITS_SINE_LUT] ];
+end
+
+endmodule
