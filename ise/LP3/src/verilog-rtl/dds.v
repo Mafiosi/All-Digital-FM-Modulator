ae661d747f653352ffef3bb2af58570424917b35
Pedro Costa <pedro.at.cst@gmail.com>
2018-12-12 14:44:27 +0000

    Notes

--- ise/LP3/src/verilog-rtl/dds.v
+++ ise/LP3/src/verilog-rtl/dds.v
@@ -1,14 +1,10 @@
 `timescale 1ns / 1ps
 
-<<<<<<< HEAD:ise/LP3/src/dds.v
-module dds #(NBITS = 8, NBITS_SINE_LUT = 8, N_OUTPUT_BITS = 8, NSAMPLES_LUT = 1024, HEXVAL = "DDSLUT.hex") (
-=======
-module dds #(NBITS = 13, NBITS_SINE_LUT = 7, N_OUTPUT_BITS = 9, NSAMPLES_LUT = 128, HEXVAL = "../simdata/DDSLUT19.hex") (
->>>>>>> 4a25fc875d8e05dd27a22606dbf193a7ec328033:ise/LP3/src/verilog-rtl/dds.v
+module dds #(NBITS_PHASE = 16, NBITS_PHASE_DEC = 8, NSAMPLES_LUT = 256, HEXVAL = "DDSLUT.hex") (
       input clock,
       input reset,
       input enableclk,
-      input [NBITS-1:0] phaseinc,
+      input [31:0] phaseinc,
       output reg [31:0] outsine
       );
 
@@ -17,15 +13,15 @@ reg [31:0] sineLUT [0:NSAMPLES_LUT-1];
 initial $readmemh(HEXVAL, sineLUT);
 
 // Defines FF for phase calculation input of LUT
-reg [NBITS-1:0] phase;
+reg [NBITS_PHASE-1:0] phase;
 always @(posedge clock)
 begin
     if (reset) begin
         phase <= 0;
     end else if (enableclk) begin
-        phase <= phase + phaseinc;
+        phase <= phase + phaseinc[NBITS_PHASE-1:0];
     end
-    outsine <= sineLUT[ phase[NBITS-1:NBITS-NBITS_SINE_LUT] ];
+    outsine <= sineLUT[ phase[NBITS_PHASE-1:NBITS_PHASE-NBITS_PHASE_FRAC] ];
 end
 
 endmodule
